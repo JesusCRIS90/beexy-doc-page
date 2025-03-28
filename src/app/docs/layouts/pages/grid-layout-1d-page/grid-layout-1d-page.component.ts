9d82e188609db3944a1853f18de72d00ee970469
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-03-28 20:05:26 +0100

    Add grid layout components and update styling in grid layout pages

--- src/app/docs/layouts/pages/grid-layout-1d-page/grid-layout-1d-page.component.ts
+++ src/app/docs/layouts/pages/grid-layout-1d-page/grid-layout-1d-page.component.ts
@@ -1,9 +1,163 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import {
+  ComboBoxComponent,
+  comboBoxOption
+} from "@beexy/ngx-inputs"
+
+import {
+  GridLayout1DComponent as GRID1D,
+  HorizontalLayoutComponent,
+  POLICY_POSITION as POLICY,
+  POLICY_ORIENTATION as ORIENTATION,
+} from "@beexy/ngx-layouts"
+
+const orientationOptions: comboBoxOption[] = [
+  {
+    internalValue: 'vert',
+    externalValue: 'VERTICAL'
+  },
+  {
+    internalValue: 'hori',
+    externalValue: 'HORIZONTAL'
+  },
+]
+
+const anchorPointOptions: comboBoxOption[] = [
+  {
+    internalValue: 'top-left',
+    externalValue: 'TOP LEFT'
+  },
+  {
+    internalValue: 'top-center',
+    externalValue: 'TOP CENTER'
+  },
+  {
+    internalValue: 'top-right',
+    externalValue: 'TOP RIGHT'
+  },
+
+  {
+    internalValue: 'center-left',
+    externalValue: 'CENTER LEFT'
+  },
+  {
+    internalValue: 'center-center',
+    externalValue: 'CENTER CENTER'
+  },
+  {
+    internalValue: 'center-right',
+    externalValue: 'CENTER RIGHT'
+  },
+
+  {
+    internalValue: 'bottom-left',
+    externalValue: 'BOTTOM LEFT'
+  },
+  {
+    internalValue: 'bottom-center',
+    externalValue: 'BOTTOM CENTER'
+  },
+  {
+    internalValue: 'bottom-right',
+    externalValue: 'BOTTOM RIGHT'
+  }
+]
 
 @Component({
   selector: 'app-grid-layout-1d-page',
-  imports: [],
+  imports: [ GRID1D, HorizontalLayoutComponent, ComboBoxComponent ],
   templateUrl: './grid-layout-1d-page.component.html',
   styleUrl: './grid-layout-1d-page.component.css',
 })
-export default class GridLayout1dPageComponent { }
+export default class GridLayout1dPageComponent {
+
+  POLICY = POLICY;
+
+  orientation = signal<ORIENTATION>(ORIENTATION.HORIZONTAL);
+  policy = signal<POLICY>(POLICY.CENTER_CENTER);
+
+  orientationCBOptions = signal<comboBoxOption[]>(orientationOptions);
+  policyCBOptions = signal<comboBoxOption[]>(anchorPointOptions);
+
+
+  updatePolicy(newPolicy: string) {
+    this.policy.set( str2Policy( newPolicy ) );
+  }
+
+  updateOrientation(newOrientation: string) {
+    this.orientation.set( str2Orientation( newOrientation ) );
+  }
+
+}
+
+function str2Orientation(input: string): ORIENTATION {
+
+  let val: ORIENTATION = ORIENTATION.HORIZONTAL;
+
+  switch (input) {
+
+    case "vert":
+      val = ORIENTATION.VERTICAL;
+      break;
+
+    case "hori":
+      val = ORIENTATION.HORIZONTAL;
+      break
+
+    default:
+      val = ORIENTATION.HORIZONTAL;
+      break
+  }
+  return val;
+}
+
+function str2Policy(input: string): POLICY {
+
+  let val: POLICY = POLICY.INHERIT;
+
+  switch (input) {
+
+    case "top-left":
+      val = POLICY.TOP_LEFT;
+      break;
+
+    case "top-center":
+      val = POLICY.TOP_CENTER;
+      break;
+
+    case "top-right":
+      val = POLICY.TOP_RIGHT;
+      break;
+
+    case "center-left":
+      val = POLICY.CENTER_LEFT;
+      break;
+
+    case "center-center":
+      val = POLICY.CENTER_CENTER;
+      break;
+
+    case "center-right":
+      val = POLICY.CENTER_RIGHT;
+      break;
+
+    case "bottom-left":
+      val = POLICY.BOTTOM_LEFT;
+      break;
+
+    case "bottom-center":
+      val = POLICY.BOTTOM_CENTER;
+      break;
+
+    case "bottom-right":
+      val = POLICY.BOTTOM_RIGHT;
+      break;
+
+
+    default:
+      val = POLICY.INHERIT;
+      break
+  }
+  return val;
+}
--- src/app/docs/layouts/pages/grid-layout-2d-page/grid-layout-2d-page.component.ts
+++ src/app/docs/layouts/pages/grid-layout-2d-page/grid-layout-2d-page.component.ts
@@ -1,9 +1,136 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import {
+  ComboBoxComponent,
+  comboBoxOption
+} from "@beexy/ngx-inputs"
+
+import {
+  GridLayout2DComponent as GRID2D,
+  ItemGridLayoutComponent as ItemGrid,
+  HorizontalLayoutComponent,
+  POLICY_POSITION as POLICY,
+} from "@beexy/ngx-layouts"
+
+const policyCBOptions: comboBoxOption[] = [
+  {
+    internalValue: 'top-left',
+    externalValue: 'TOP LEFT'
+  },
+  {
+    internalValue: 'top-center',
+    externalValue: 'TOP CENTER'
+  },
+  {
+    internalValue: 'top-right',
+    externalValue: 'TOP RIGHT'
+  },
+
+  {
+    internalValue: 'center-left',
+    externalValue: 'CENTER LEFT'
+  },
+  {
+    internalValue: 'center-center',
+    externalValue: 'CENTER CENTER'
+  },
+  {
+    internalValue: 'center-right',
+    externalValue: 'CENTER RIGHT'
+  },
+
+  {
+    internalValue: 'bottom-left',
+    externalValue: 'BOTTOM LEFT'
+  },
+  {
+    internalValue: 'bottom-center',
+    externalValue: 'BOTTOM CENTER'
+  },
+  {
+    internalValue: 'bottom-right',
+    externalValue: 'BOTTOM RIGHT'
+  }
+]
 
 @Component({
   selector: 'app-grid-layout-2d-page',
-  imports: [],
+  imports: [ GRID2D, HorizontalLayoutComponent, ComboBoxComponent, ItemGrid ],
   templateUrl: './grid-layout-2d-page.component.html',
   styleUrl: './grid-layout-2d-page.component.css',
 })
-export default class GridLayout2dPageComponent { }
+export default class GridLayout2dPageComponent { 
+
+  POLICY = POLICY;
+
+  policy = signal<POLICY>(POLICY.CENTER_CENTER);
+  policy_second = signal<POLICY>(POLICY.CENTER_CENTER);
+  policy_itemGrid = signal<POLICY>(POLICY.CENTER_CENTER);
+
+  policyCBOptions = signal<comboBoxOption[]>(policyCBOptions);
+
+
+  updatePolicy(newPolicy: string) {
+    this.policy.set( str2Policy( newPolicy ) );
+  }
+
+  updatePolicySecond(newPolicy: string) {
+    this.policy_second.set( str2Policy( newPolicy ) );
+  }
+
+  updatePolicyIndividualItem(newPolicy: string) {
+    this.policy_itemGrid.set( str2Policy( newPolicy ) );
+  }
+
+}
+
+
+function str2Policy(input: string): POLICY {
+
+  let val: POLICY = POLICY.INHERIT;
+
+  switch (input) {
+
+    case "top-left":
+      val = POLICY.TOP_LEFT;
+      break;
+
+    case "top-center":
+      val = POLICY.TOP_CENTER;
+      break;
+
+    case "top-right":
+      val = POLICY.TOP_RIGHT;
+      break;
+
+    case "center-left":
+      val = POLICY.CENTER_LEFT;
+      break;
+
+    case "center-center":
+      val = POLICY.CENTER_CENTER;
+      break;
+
+    case "center-right":
+      val = POLICY.CENTER_RIGHT;
+      break;
+
+    case "bottom-left":
+      val = POLICY.BOTTOM_LEFT;
+      break;
+
+    case "bottom-center":
+      val = POLICY.BOTTOM_CENTER;
+      break;
+
+    case "bottom-right":
+      val = POLICY.BOTTOM_RIGHT;
+      break;
+
+
+    default:
+      val = POLICY.INHERIT;
+      break
+  }
+  return val;
+}
