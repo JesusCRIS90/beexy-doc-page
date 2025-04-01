0b8501188216acfc1be11a69846bee50e77a97b3
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-04-01 13:08:53 +0200

    Implement responsive card grid layout with adjustable card amount and inner window size

--- src/app/docs/layouts/pages/responsive-card-grid-layout-page/responsive-card-grid-layout-page.component.ts
+++ src/app/docs/layouts/pages/responsive-card-grid-layout-page/responsive-card-grid-layout-page.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+import { NumericalSliderComponent } from "@beexy/ngx-inputs"
+
+import { 
+  CardLayoutComponent, 
+  ResponsiveCardGridLayoutComponent, 
+  HorizontalLayoutComponent,
+  FixedWidthLayoutComponent,
+  POLICY_POSITION as POLICY
+ } from "@beexy/ngx-layouts"
 
 @Component({
   selector: 'app-responsive-card-grid-layout-page',
-  imports: [],
+  imports: [
+    CardLayoutComponent,
+    ResponsiveCardGridLayoutComponent,
+    HorizontalLayoutComponent,
+    FixedWidthLayoutComponent,
+    NumericalSliderComponent
+  ],
   templateUrl: './responsive-card-grid-layout-page.component.html',
   styleUrl: './responsive-card-grid-layout-page.component.css',
 })
-export default class ResponsiveCardGridLayoutPageComponent { }
+export default class ResponsiveCardGridLayoutPageComponent {
+
+  POLICY = POLICY;
+
+  innerWindowSize = signal<number>(1800);
+  cardsAmount = signal<number>(1);
+  
+  cardsNumberArray = computed(() =>
+    Array.from({ length: this.cardsAmount() }, (_, i) => i)
+  );
+
+
+  updateInnerWindowSize(newInnerWidth: number) {
+    this.innerWindowSize.set( newInnerWidth );
+    // console.log(this.cardWidth())
+  }
+
+  updateCardAmount(newAmount: number) {
+    this.cardsAmount.set( newAmount );
+  }
+
+}
